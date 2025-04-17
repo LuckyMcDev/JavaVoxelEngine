1a7d72962f3634eba160a8ed1794419ca02b58f2
LuckyMcDev <lucky.dev@myyahoo.com>
2025-04-17 14:11:04 +0200

    add more Leaf types

--- core/src/main/java/net/fynn/javavoxelengine/world/VoxelType.java
+++ core/src/main/java/net/fynn/javavoxelengine/world/VoxelType.java
@@ -6,13 +6,29 @@ import com.badlogic.gdx.graphics.Color;
  * Enthält die verschiedenen Voxeltypen mit ihren Farben und Sichtbarkeitseigenschaften.
  */
 public enum VoxelType {
-    AIR(null),  // AIR ist unsichtbar
-    STONE(new Color(0.5f, 0.5f, 0.5f, 1f)),
-    DIRT(new Color(0.545f, 0.271f, 0.075f, 1f)),
-    GRASS(new Color(0.133f, 0.545f, 0.133f, 1f)),
-    WOOD(new Color(0.1f,0.4f,0.1f,1f)),
-    LEAVES(new Color(1f,1f,1f,1f));
 
+    /** Luft, unsichtbar. */
+    AIR(null),
+
+    /** Stein, grau. */
+    STONE(new Color(0.5f, 0.5f, 0.5f, 1f)),
+
+    /** Erde, braun. */
+    DIRT(new Color(0.545f, 0.271f, 0.075f, 1f)),
+
+    /** Gras, grün. */
+    GRASS(new Color(0.133f, 0.545f, 0.133f, 1f)),
+
+    /** Holz, braun. */
+    WOOD(new Color(0.361f,0.329f,0.239f,1f)),
+
+    /** Blätter, Hellgrün. */
+    LEAVES_LIGHT(new Color(0.514f,0.851f,0.447f,1f)),
+
+    /** Blätter, Dunkelgrün. */
+    LEAVES_DARK(new Color(0.298f,0.602f,0.259f,1f));
+
+    /** Die Farbe des Voxeltyps. */
     public final Color color;
 
     VoxelType(Color color) {
